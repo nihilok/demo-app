1963806d591715c1b00f7b208fb34e028b75e670
nihilok <t0k3n8l4ck@gmail.com>
2020-10-04 00:18:10 +0100

    Update main.py

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ class MainApp(MDApp):
                      "emoticon-devil-outline", "emoticon-excited-outline", "emoticon-happy-outline",
                      "emoticon-kiss-outline", "emoticon-neutral-outline", "emoticon-neutral", "emoticon-poop",
                      "emoticon-sad"]
-		return icon_list
+		    return icon_list
 
 if __name__ == '__main__':
     MainApp().run()
